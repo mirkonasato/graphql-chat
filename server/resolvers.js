de28c09ef316bafed0603f262ef8b69eab374467
Mirko Nasato <mirko@nasato.dev>
2022-04-27 18:29:47 +0100

    Subscription Resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,7 @@
+import { PubSub } from 'graphql-subscriptions';
 import { Message } from './db.js';
+
+const pubSub = new PubSub();
 
 function rejectIf(condition) {
   if (condition) {
@@ -15,9 +18,17 @@ export const resolvers = {
   },
 
   Mutation: {
-    addMessage: (_root, { input }, { userId }) => {
+    addMessage: async (_root, { input }, { userId }) => {
       rejectIf(!userId);
-      return Message.create({ from: userId, text: input.text });
+      const message = await Message.create({ from: userId, text: input.text });
+      pubSub.publish('MESSAGE_ADDED', { messageAdded: message });
+      return message;
     },  
+  },
+
+  Subscription: {
+    messageAdded: {
+      subscribe: () => pubSub.asyncIterator('MESSAGE_ADDED'),
+    },
   },
 };
